98eb085fc05cf34281a72f1c8291eb95921fc5c8
emre.ast <emre_ast@hotmail.com>
2022-04-01 16:57:14 +0300

    Codes from first lecture Basic DOM manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,12 @@
 'use strict';
 
+//First Lecture
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 7;
+document.querySelector('.score').textContent = 15;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 11;
+console.log(document.querySelector('.guess').value);
