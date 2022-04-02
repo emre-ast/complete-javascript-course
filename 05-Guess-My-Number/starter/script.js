74a469759f47a2e30d1f7f1221acc635358fd0a2
ma226fq <ma226fq@student.lnu.se>
2022-04-02 21:32:18 +0300

    Lecture 2

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //First Lecture
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
 
@@ -10,3 +11,30 @@ document.querySelector('.score').textContent = 15;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 11;
 console.log(document.querySelector('.guess').value);
+*/
+
+//Lecture 2
+const luckyNumber = Math.trunc(Math.random() * 20) + 1;
+
+const updateScore = () =>
+  (document.querySelector('.score').textContent =
+    Number(document.querySelector('.score').textContent) - 1);
+
+function guessTheNumber() {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (guess == luckyNumber) {
+    document.querySelector('.message').textContent = 'Correct!';
+    document.querySelector('.number').textContent = luckyNumber;
+  } else if (guess < luckyNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
+    updateScore();
+  } else if (guess > luckyNumber) {
+    document.querySelector('.message').textContent = 'Too High!';
+    updateScore();
+  } else {
+    document.querySelector('.message').textContent = 'Enter a Number';
+  }
+}
+
+document.querySelector('.check').addEventListener('click', guessTheNumber);
