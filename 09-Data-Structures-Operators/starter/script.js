cf28194eb5e23e1483a34122af63999560780535
emre.ast <emre_ast@hotmail.com>
2022-04-05 17:11:32 +0300

    Operations on data structures

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,108 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+console.log('---- OR ----');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+/*
+//Rest Pattern
+const sum = function (...inputs) {
+  let sum = 0;
+  inputs.forEach(element => (sum += element));
+
+  return sum;
+};
+
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+*/
+
+/*
+//Spread Operator (...)
+const arr = [1, 2, 3];
+const badNewArr = [arr[0], arr[1], arr[2], 4, 5];
+const newArr = [...arr, 4, 5];
+
+console.log(...badNewArr);
+console.log(...newArr);
+*/
+
+/*
+//Destructing Object
+const { name: restaurantName, location: loc, categories: tags } = restaurant;
+console.log(restaurantName, loc, tags);
+
+//Mutating variables
+let a = 222;
+let b = 333;
+
+const obj = { a: 6, b: 8, c: 10 };
+
+//You need to wrap this statement in paranthesises otherwise compiler compile it as a code block
+({ a, b } = obj);
+
+console.log(a, b);
+
+//Nested Obj
+const {
+  openingHours: {
+    thu,
+    fri: { open, close },
+  },
+} = restaurant;
+console.log(thu);
+console.log(open, close);
+*/
+
+/*
+const array = [3, 4, 5];
+
+//Destructing Array
+const [x, y, z] = array;
+console.log(x, y, z);
+
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//Switching values by destructing
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [starterCourse, mainCourse] = restaurant.order(2, 1);
+console.log(starterCourse, mainCourse);
+
+const nested = [4, 5, [6, 7]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+*/
